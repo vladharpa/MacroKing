cc0cc4a611989e1aab51383bfebe485788d48706
Andrei Harpa <contact@andreiharpa.dev>
2024-03-24 19:23:27 +0200

    Added example on how to render view based on props

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -5,37 +5,37 @@ import  path  from "path";
 
 
 const fileDirectory = path.resolve('src','views');
-app.get('/register',async (req,res)=>{
-    res.render('register.ejs',{root:fileDirectory });
+app.get('/register', async (req,res) => {
+    res.render('register.ejs', {inUse: false});
     
-})
+});
 
 app.post('/register',async (req,res)=>{
-    let inUse=true;
-    const UserDB = await Users.find();
-    for(let user of UserDB)
-    {
-        if(user.email==req.body.email)
-        {
-            inUse=false;   
-            console.log('Email already used');
-            res.render('register.ejs',{inUse});
+    // let inUse=true;
+    // const UserDB = await Users.find();
+    // for(let user of UserDB)
+    // {
+    //     if(user.email==req.body.email)
+    //     {
+    //         inUse=false;   
+    //         console.log('Email already used');
+    //         res.render('register.ejs',{inUse});
             
-        }
-        if(user.password==req.body.password)
-        {
+    //     }
+    //     if(user.password==req.body.password)
+    //     {
 
-            inUse=false;
-            console.log('Password already used');
-            res.render('register.ejs',{inUse});
+    //         inUse=false;
+    //         console.log('Password already used');
+    //         res.render('register.ejs',{inUse});
             
-        }
-    }
-    if(inUse==true)
-    {
-        console.log("Success");
-        Users.create({email:req.body.email , password:req.body.password});
-        res.render('register.ejs',{inUse});
-    }
-
+    //     }
+    // }
+    // if(inUse==true)
+    // {
+    //     console.log("Success");
+    //     Users.create({email:req.body.email , password:req.body.password});
+    //     res.render('register.ejs',{inUse});
+    // }
+    res.render('register.ejs', { inUse: true });
 })
