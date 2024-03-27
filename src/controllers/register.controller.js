9cb89ba25f4f5b0b7a77482a14c4f11e1c00d30b
vladharpa <harpa.vlad@gmail.com>
2024-03-27 21:58:25 +0200

    Made the registration algorithm more efficient and added confirm password logic

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -7,40 +7,40 @@ import  path  from "path";
 const fileDirectory = path.resolve('src','views');
 
 app.get('/register', async (req,res) => {
-    res.render('register', {inUse: false});
+    res.render('register', {inUse:undefined});
     
 });
 
 app.post('/register',async (req,res)=>{
-    let inUse=false;
-    const UserDB = await Users.find();
-    for(let user of UserDB)
+    
+    let EmailUsed=await Users.find({email:req.body.email});
+    let PasswordUsed=await Users.find({password:req.body.password});
+    if(EmailUsed[0]==undefined && PasswordUsed[0]==undefined)
     {
-        if(user.email==req.body.email)
-        {
-            inUse=true;   
-            console.log('erroare');
-            res.render('./register.ejs',{inUse:inUse});
-            
+        if(req.body.password!=req.body.confirm_password){
+            console.log('confirm error');
+            res.render('register.ejs',{inUse:"ConfirmareEsuata"})
         }
-        if(user.password==req.body.password)
-        {
-
-            inUse=false;
-            console.log('erroare');
-            res.render('./register.ejs',{inUse:inUse});
-            
+        else{
+            console.log('Uraaa');
+            Users.create({email:req.body.email,password:req.body.password})
+            res.sendFile("registerSuccesfull.html",{root:'src/public'})
         }
-    }
-    if(inUse==false)
-    {
-        console.log("succes");
-        await Users.create({email:req.body.email , password:req.body.password});
-        res.sendFile('/registerSuccesfull.html',{root:'src/public'});
         
     }
-
-
     
+    if(EmailUsed[0]!=undefined && PasswordUsed[0]==undefined)
+    {
+        console.log('email error');
+        res.render("register.ejs",{inUse:"email"});
+    }
+    if(EmailUsed[0]==undefined && PasswordUsed[0]!=undefined)
+    {
+        console.log('password error');
+        res.render("register.ejs",{inUse:"password"});
+    }
+    
+     
+
 })
 
