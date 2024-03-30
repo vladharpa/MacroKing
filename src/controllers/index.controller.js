021e604f0d00f9a43b4a206d3ab525cf05b36c3b
vladharpa <harpa.vlad@gmail.com>
2024-03-30 15:03:45 +0200

    Tasks solved

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,6 @@
 import { app } from "../config/express.js";
 import  path  from "path";
 
-app.get('/index',async (req,res,next)=>{
+app.get('/',async (req,res,next)=>{
     res.render('index');
 })
--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,5 +1,9 @@
 import { app } from "../config/express.js";
 import  path  from "path";
+app.get('/login',(req,res)=>{
+
+    res.render('mainpage.ejs');
+})
 
 app.post('/login',async (req,res)=>{
     res.sendFile()
