9eb81eba3d5de4a38a20ee29d2252da00694e595
vladharpa <harpa.vlad@gmail.com>
2024-04-20 13:37:24 +0300

    added the remove buttons,without functionality

--- src/public/js/newRecipe.js
+++ src/public/js/newRecipe.js
@@ -1,19 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
     const btn = document.getElementById("addIngredient");
     btn.addEventListener('click',(evt)=>{
         evt.preventDefault();
-        Addbtn();
-    });
+        addInput();
 
-    function Addbtn(){
+    });
+    
+    function addInput(){
+        
         let div = document.getElementById("interesting-shape");
         var currentHeight = div.offsetHeight;
         var newHeight = currentHeight + 40; // Increase the height by 20 pixels (you can adjust this value as needed)
         div.style.height = newHeight + "px";
-        let newInput = document.createElement("input");
-        newInput.type="text";
-        newInput.name="ingredients[]";
-        newInput.classList.add("newIngredient");
-        btn.insertAdjacentElement("afterend", newInput);
+        let field=document.createElement("input");
+        field.type="text";
+        field.name="ingredients[]";
+        field.classList.add("newIngredient");
+        let btnRemove=document.createElement('button');
+        btnRemove.classList.add('removeBtn');
+        btn.insertAdjacentElement("afterend",btnRemove);
+        btn.insertAdjacentElement("afterend",field);
     }  
-});
+
+
